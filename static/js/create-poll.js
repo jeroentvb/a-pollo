5e2930289f7065061f022683aecef1c5c4f45fd7
jeroentvb <jeroentvb@hotmail.com>
2019-03-27 12:59:54 +0100

    Rewritten to es2015 for better browser support

--- static/js/create-poll.js
+++ static/js/create-poll.js
@@ -1,34 +1,36 @@
 (() => {
-  const options = document.getElementById('options')
-  const button = document.getElementById('more-options')
-  let currentNewOption = 3
+  var options = document.getElementById('options')
+  var button = document.getElementById('more-options')
+  var currentNewOption = 3
 
   button.value = 'Add option'
 
-  for (let i = 3; i < 6; i++) {
-    document.getElementById(`option-${i}-label`).remove()
-    document.getElementById(`option-${i}`).remove()
+  for (var i = 3; i < 6; i++) {
+    document.getElementById('option-' + i + '-label').remove()
+    document.getElementById('option-' + i).remove()
   }
 
-  button.addEventListener('click', e => {
+  button.addEventListener('click', function (e) {
     e.preventDefault()
 
-    const label = document.createElement('label')
-    label.id = `option-${currentNewOption}-label`
-    label.setAttribute('for', `option-${currentNewOption}`)
-    const labelText = document.createTextNode(`Option ${currentNewOption}`)
+    var label = document.createElement('label')
+    label.id = 'option-' + currentNewOption + '-label'
+    label.setAttribute('for', 'option-' + currentNewOption)
+    var labelText = document.createTextNode('Option' + currentNewOption)
     label.appendChild(labelText)
 
-    const option = document.createElement('input')
+    var option = document.createElement('input')
     option.setAttribute('type', 'text')
-    option.id = `option-${currentNewOption}`
-    option.setAttribute('name', `option${currentNewOption}`)
+    option.id = 'option-' + currentNewOption
+    option.setAttribute('name', 'option' + currentNewOption)
 
     options.appendChild(label)
     options.appendChild(option)
 
     currentNewOption++
 
-    if (currentNewOption === 11) document.getElementById('more-options').remove()
+    if (currentNewOption === 11) {
+      document.getElementById('more-options').remove()
+    }
   })
 })()
